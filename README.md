aff8356cbdb0777b1225ab9f93e60aaaccd40f2c
JosephZhou <zhoucj98@gmail.com>
2016-10-28 15:31:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# pdf2img.py
+
+## 环境配置
+```
+pip install wand
+pip install PyPDF2
+yum install ImageMagick -y
+```
+
+## 运行
+```
+python pdf2img.py ur_filename
+```
+
+OR
+
+```
+python pdf2img.py ur_path/ur_filename
+```
---
+++ pdf2img.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from wand.image import Image
+from PyPDF2 import PdfFileReader
+import logging
+import os,re,sys
+import warnings
+
+
+def process(source):
+    # 取文件名
+    file_path = os.path.splitext(source)[0]
+    input_file = PdfFileReader(file(source,'rb'))
+    pages = input_file.getNumPages()
+    path = os.path.split(file_path)
+
+    logging.info('')
+    for i in range(pages):
+        with Image(filename=source + '[' + str(i) + ']',resolution=200) as converted:
+            converted.compression_quality = 45
+            newfile = file_path + '-' + str(i + 1) + '.png'
+            converted.save(filename=newfile )
+            logging.info(newfile)
+
+    # 验证文件数量
+    # 取文件名
+    p = re.compile(os.path.splitext(path[1])[0])
+    all_file = ','.join(os.listdir(path[0]))
+    if len(p.findall(all_file)) - 1 == pages:
+        print 'Page Size:%s' % (pages)
+    else:
+        print pages
+        logging.warning(' '.join(os.listdir(path[0])))
+
+if __name__ == '__main__':
+    try:
+        # 禁用警告
+        warnings.filterwarnings('ignore')
+
+        # 设置logging
+        if not os.path.exists('logs'): os.mkdir('logs')
+        log_filename = 'logs/pdf2img.log'
+        logging.basicConfig(filename=log_filename, filemode='a', level=logging.DEBUG,
+                            format = '%(asctime)s %(filename)s [%(levelname)s] %(message)s',
+                            datefmt = '[%Y-%m-%d %H:%M:%S]',
+                            )
+
+        #get_source = 'pdf2img/de5553b0e452e16a099e1ac3844621d5.pdf'
+        get_source = sys.argv[1]
+        if get_source:
+            process(get_source)
+        else:
+            logging.error('no file')
+    except Exception,e:
+        print 'Page Size:0'
