ebf2a775ec2ce82d92bc71e3125c5fc9bb18b763
JosephZhou <zhoucj98@gmail.com>
2016-11-07 17:13:12 +0800

    加入线程处理，默认4线程

--- pdf2img.py
+++ pdf2img.py
@@ -6,22 +6,20 @@ from PyPDF2 import PdfFileReader
 import logging
 import os,sys
 import warnings
+import threading
 
 
-def process(source):
-    # 取文件名
+def process(source,beginning,ending):
+    # 取文件名（test/test1/ab）
     file_path = os.path.splitext(source)[0]
-    input_file = PdfFileReader(file(source,'rb'))
-    pages = input_file.getNumPages()
-    logging.info('')
-    for i in range(pages):
+    logging.info('=========>>> Start')
+    for i in range(beginning,ending):
         with Image(filename=source + '[' + str(i) + ']',resolution=200) as converted:
             converted.compression_quality = 45
-            newfile = file_path + '-' + str(i + 1) + '.png'
+            newfile = file_path + '-' + str(i + 1) + '.jpg'
             converted.save(filename=newfile )
-            logging.info(newfile)
+            logging.info(threading.current_thread().getName() + ' ' + newfile)
     if not os.path.isfile(newfile): logging.warning('page of file is not found:[%s]' % (newfile))
-    print 'Page Size:%s' % (pages)
 
 if __name__ == '__main__':
 
@@ -39,7 +37,30 @@ if __name__ == '__main__':
     try:
         # get_source = 'test/test1/ab.pdf'
         get_source = sys.argv[1]
-        process(get_source)
+        # 线程数
+        threads = 4
+        # 获取页数
+        input_file = PdfFileReader(file(get_source, 'rb'))
+        pages = input_file.getNumPages()
+        step = pages / threads
+        ending_page = 0
+        # 线程
+        threads_list = []
+        for i in range(threads):
+            beginning_page = ending_page
+            ending_page = beginning_page + step
+            t = threading.Thread(target=process,args=(get_source,beginning_page,ending_page))
+            threads_list.append(t)
+        # 启动线程
+        for t in threads_list:
+            t.setDaemon(True)
+            t.start()
+        t.join()
+        process(get_source,ending_page,pages)
+        print 'Page Size:%s' % (pages)
+        logging.info('<<<========= End')
     except Exception,e:
         logging.error(e)
         print 'Page Size:0'
+
+
